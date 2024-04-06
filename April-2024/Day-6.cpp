a81efa2614634e46aa0ed513ad7b85ea4d0d2f1a
Samarth Verma <samarthverma2804@gmail.com>
2024-04-06 23:43:36 +0530

    april day-6 solution added

---
+++ April-2024/Day-6.cpp
@@ -0,0 +1,7 @@
+# Count ways to N'th Stair #
+# Problem Link: https://www.geeksforgeeks.org/problems/count-ways-to-nth-stairorder-does-not-matter1322/1
+long long countWays(int n) {
+        long long ans=0;
+        ans=(n/2)+1;
+        return ans;
+}
