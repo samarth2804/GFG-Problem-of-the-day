1faf1e6aa2e43d812f43a1bfd205733d61240074
Samarth Verma <samarthverma2804@gmail.com>
2024-04-08 22:26:12 +0530

    april day-8 solution addded

---
+++ April-2024/Day-8.cpp
@@ -0,0 +1,31 @@
+# Optimal Strategy For A Game #
+# Problem Link: https://www.geeksforgeeks.org/problems/optimal-strategy-for-a-game-1587115620/1
+
+class Solution{
+    public:
+long long maximumAmount(int n, int arr[]) 
+{ 
+    //creating a table to store solutions of subproblems.
+    int table[n][n]; 
+  
+    //filling table using above recursive formula. Note that the 
+    //table is filled in diagonal fashion, from diagonal elements 
+    //to table[0][n-1] which is the result. 
+    for (int gap = 0; gap < n; ++gap) 
+    { 
+        for (int i = 0, j = gap; j < n; ++i, ++j) 
+        { 
+            //Here x is value of table[i + 2][j], y istable[i + 1][j - 1] and 
+            // z is table[i][j - 2] in above recursive formula.
+            
+            int x = ((i + 2) <= j) ? table[i + 2][j] : 0; 
+            int y = ((i + 1) <= (j - 1)) ? table[i + 1][j - 1] : 0; 
+            int z = (i <= (j - 2)) ? table[i][j - 2] : 0; 
+  
+            table[i][j] = max(arr[i] + min(x, y), arr[j] + min(y, z)); 
+        } 
+    } 
+    //returning the result.
+    return table[0][n - 1]; 
+} 
+};
